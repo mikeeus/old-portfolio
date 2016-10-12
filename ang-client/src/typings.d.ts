9e687108b086be7360d76a3e9f1da376848fc96c
mikeeus <itsmikias@gmail.com>
2016-10-12 17:24:37 +0300

    Add NodeRequire Interface, return app.js to normal

--- ang-client/src/typings.d.ts
+++ ang-client/src/typings.d.ts
@@ -2,5 +2,9 @@
 // https://github.com/typings/typings
 // https://www.typescriptlang.org/docs/handbook/writing-declaration-files.html
 
+interface NodeRequire {
+    ensure: (paths: string[], callback: (require: <T>(path: string) => T) => void, name?:string ) => void;
+}
+
 declare var System: any;
-declare var require;
+declare var require: NodeRequire;
--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var cors = require('cors');
 
 var routes = require('./routes/index');
 // var users = require('./routes/users');
@@ -23,7 +22,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'ang-client/dist')));
-app.use(cors());
 
 app.use('/', routes);
 
