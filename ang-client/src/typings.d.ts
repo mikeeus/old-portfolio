0a80beb79bd89e59c6e991ae5f5384408f0b6c79
mikeeus <itsmikias@gmail.com>
2016-10-14 21:32:07 +0300

    Update type

--- ang-client/src/typings.d.ts
+++ ang-client/src/typings.d.ts
@@ -2,9 +2,9 @@
 // https://github.com/typings/typings
 // https://www.typescriptlang.org/docs/handbook/writing-declaration-files.html
 
-interface NodeRequire {
-    ensure: (paths: string[], callback: (require: <T>(path: string) => T) => void, name?:string ) => void;
-}
+// interface NodeRequire {
+//     ensure: (paths: string[], callback: (require: <T>(path: string) => T) => void, name?:string ) => void;
+// }
 
 declare var System: any;
-declare var require: NodeRequire;
+declare var require;
