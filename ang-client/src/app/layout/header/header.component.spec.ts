dd433323879106935eda3521c68eb239554630a0
mikeeus <itsmikias@gmail.com>
2016-10-21 12:49:44 +0300

    Add nyan test runner

--- ang-client/src/app/layout/header/header.component.spec.ts
+++ ang-client/src/app/layout/header/header.component.spec.ts
@@ -2,10 +2,20 @@
 
 import { TestBed, async } from '@angular/core/testing';
 import { HeaderComponent } from './header.component';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Store } from '@ngrx/store';
 
 describe('Component: Header', () => {
+  let storeSpy: BehaviorSubject<Store<any>>;
+  let store: Store<any>;
+
+  beforeEach(() => {
+    // use store as 'spy'
+    storeSpy = new BehaviorSubject(store);
+  })
+
   it('should create an instance', () => {
-    let component = new HeaderComponent();
+    let component = new HeaderComponent(store);
     expect(component).toBeTruthy();
   });
 });
